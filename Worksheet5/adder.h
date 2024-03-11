d3758e48ae517d41ba2a0470c1ebfac8edb9328f
Hassan Jouni <eeyhj2@nottingham.ac.uk>
2024-03-11 01:59:13 +0000

    solved worksheet5

---
+++ Worksheet5/adder.h
@@ -0,0 +1,20 @@
+// begin - adder.h --------------
+
+#ifndef MATHSLIB_ADDER_H
+#define MATHSLIB_ADDER_H
+
+/** @file
+* This file contains the declarations of all exported functions in the maths library
+*/
+
+/** Adding function
+* This function adds two numbers
+* @param a is the first number
+* @param b is the second number
+* @return sum of a and b
+*/
+
+int add(int a, int b);
+
+#endif
+// end - adder.h --------------
